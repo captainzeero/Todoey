107b3c25045444b059ae4d5d9f799a0ca071d263
Dheeraj C <mr.d@dheerajs-macbook-air.local>
2018-05-08 14:16:25 +0530

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Dheeraj M C on 07/05/18.
+//  Copyright © 2018 Dheeraj M C. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false 
+}
